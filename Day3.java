f493ca7443adb78aaca3c4c962c9242f75f41350
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-10 06:48:23 +0700

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,19 @@
+package pkg100days;
+public class Day3 {
+    public static void main(String[] args) {
+     //untuk komentar 1 baris    
+     //komentar untuk menampilkan teks      
+       System.out.println("hari ke3"); 
+       
+     //komentar untuk menampilkan penjumlahan menggunakan tipe data int
+       int angka = 4;
+       System.out.println("angka : " + (angka + 3));
+       
+    //ini komentar untuk teks panjang       
+    /*Semoga saya bisa menyelesaikan tantangan 100 Days of Coding 
+       dengan baik dan konsisten. Saya berharap setiap hari yang 
+       saya lalui dapat menjadi pengalaman berharga untuk menambah 
+       pengetahuan, keterampilan, dan kedisiplinan dalam dunia pemrograman.*/       
+       System.out.println("semoga konsisten ngoding");
+    }
+}
