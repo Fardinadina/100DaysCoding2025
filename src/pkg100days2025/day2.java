b4a1796aac7831a3d9a1735abd1bb66b7a00c5ad
Fardinadina <fardinadina936@gmail.com>
2025-09-09 14:58:59 +0700

    Hari Kedua

---
+++ src/pkg100days2025/day2.java
@@ -0,0 +1,12 @@
+package pkg100days2025;
+
+public class day2 {
+
+    public static void main(String[] args) {
+        System.out.println("satu");
+        System.out.print("satu");
+        System.out.printf(" nama saya fardina dan umur saya 19th.%n");
+        System.out.printf("hobi saya menggosip.%n");
+    }
+    
+}
---
+++ src/pkg100days2025/day3.java
@@ -0,0 +1,8 @@
+package pkg100days2025;
+public class day3 {
+    public static void main(String[] args) {
+        
+    System.out.println("kode day 3 nih"); //untuk komentarnya
+  //System.out.println("ini kode untuk dikomentari");
+      }
+    }
