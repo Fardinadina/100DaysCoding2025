377403ee9215770460b3020d571020b12ac37782
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-11 09:05:33 +0700

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,12 @@
+package pkg100days;
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.print("Universitas \nSulawesi Barat \n");
+        System.out.println("nama\t:\tfardina");
+        System.out.println("nim\t:\tD0223312");
+        System.out.print("kelas\t:\t\"Informatika A\"\n");
+        System.out.println("c//document//iscBerantasButaCoding");
+        
+    }
+    
+}
