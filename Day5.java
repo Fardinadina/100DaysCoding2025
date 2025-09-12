dcacef64bf8e722f86429cf4005868507ff9dea3
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-12 19:41:33 +0700

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,19 @@
+
+package pkg100days;
+
+public class Day5 {
+    
+    public static void main(String[] args) {
+        byte umur = 20;
+        short kodepos = 30140;
+        int hargaobathaus = 10000;
+        long hutangnegara = 11000000000000L; 
+
+        System.out.println("Byte  : " + umur);
+        System.out.println("Short : " + kodepos);
+        System.out.println("Int   : " + hargaobathaus);
+        System.out.println("Long  : " + hutangnegara);
+    }
+}
+
+
