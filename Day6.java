c06bb98e1180911e4697f695fc9b495362bef07f
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-13 08:54:56 +0700

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,20 @@
+package pkg100days;
+
+public class Day6 {
+    public static void main(String[] args) {
+
+        // Berat badan pakai tipedata double, karena biasanya pakai bilangan desimal
+        double beratBadan = 43.00; // dlm satuan kg
+
+        // Tinggi badan pakai tipedata float, cukup tepat untuk angka 1-3 meteran
+        float tinggiBadan = 1.45f; // dlm satuan meter
+
+        System.out.println("`` Data Pasien Fardina``");
+        System.out.println("Berat Badan (double): " + beratBadan + " kg");
+        System.out.println("Tinggi Badan (float): " + tinggiBadan + " m");
+  
+    }
+}
+
+    
+
