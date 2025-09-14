1b80124c9060a13175f41f48494ccdee0a3f439d
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-14 09:47:38 +0700

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,14 @@
+
+package pkg100days;
+
+public class Day7 {
+    public static void main(String[] args) {
+        char nilai = 'A';
+        boolean inibetul = true;
+        boolean inisalah = false;
+        
+        System.out.println("``Nilai matkul pbo : " + nilai +"``");
+        System.out.println("Nilainya  betul : " + inibetul);
+        System.out.println("Nilainya  salah : " + inisalah);
+    }
+}
