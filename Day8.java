912f2a93f17df4b51118152e7917bf594340a09c
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-15 09:48:39 +0700

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,9 @@
+
+package pkg100days;
+
+public class Day8 {
+    public static void main(String[] args) {
+        String FWB = "project harus dikumpul tepat waktu dan diasistensikan";
+        System.out.println("TUGAS FWB : "+FWB);
+    }
+}
