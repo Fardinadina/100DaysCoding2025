b48c2709b85b72f4b5d5ab36a7b27ff0f4021d1e
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-16 11:22:21 +0700

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,24 @@
+package pkg100days;
+
+public class Day9 {
+    public static void main(String[] args) {
+        System.out.println("==========TIPE DATA int=========");
+        int fwb = 3;
+        System.out.println("angka sebelum diubah\t :"  + fwb);
+        fwb = 4;
+        System.out.println("angka setelah diubah\t :" + fwb);
+        
+        System.out.println("==========TIPE DATA char=========");
+        char nilai= 'A';
+        System.out.println("huruf sebelum diubah\t :"  + nilai);
+        nilai = 'F';
+        System.out.println("huruf setelah diubah\t :" + nilai);
+        
+        System.out.println("==========TIPE DATA String=========");
+        String web = "pemrograman web023";
+        System.out.println("teks sebelum diubah\t :"  + web);
+        web ="pw";
+        System.out.println("teks setelah diubah\t :" + web);
+    }
+    
+}
