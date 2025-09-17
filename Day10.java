7d7c3fa92fbba0751141087e273d3e75740d08de
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-17 10:17:52 +0700

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,10 @@
+
+package pkg100days;
+
+public class Day10 {
+    public static void main(String[] args) {
+       final double bb = 43.2;
+       System.out.println("berat badan me  : " + bb);
+       // bb = 45.10; Ketika dia constanta maka tidak bisa di update
+    }
+}
