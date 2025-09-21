b2cdd919a3a1890fc12e28e388644d0213d04096
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-21 14:23:17 +0700

    Add files via upload

---
+++ Day14.java
@@ -0,0 +1,16 @@
+package pkg100days;
+public class Day14 {
+
+    public static void main(String[] args) {
+        int umur = 20;            
+        double berat = 55.7;      
+        boolean pelajar = true; 
+
+        System.out.println("Umur: " + umur);
+        System.out.println("Berat: " + berat);
+        System.out.println("Apakah Dia Mahasiswa?" + pelajar);
+    }
+}
+
+   
+
