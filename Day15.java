7e3004bdf7977e0c6df4c2f8b5afaf3d6e3d3f01
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-22 07:08:22 +0700

    Add files via upload

---
+++ Day15.java
@@ -0,0 +1,18 @@
+
+package pkg100days;
+
+public class Day15 {
+   
+    public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 4;
+
+        int jumlah = a1 + a2;
+        int kurang = a1 - a2;
+        System.out.println("===OPERASI ARITMATIKA===");
+        System.out.println("Penjumlahan : " + jumlah);
+        System.out.println("Pengurangan : " + kurang);
+    }
+}
+
+
