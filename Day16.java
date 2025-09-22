6ca2184566f6251a49dea6bb97dd7da3acd8f648
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-22 23:58:58 +0700

    Add files via upload

---
+++ Day16.java
@@ -0,0 +1,18 @@
+
+package pkg100days;
+
+public class Day16 {
+   
+    public static void main(String[] args) {
+        int a1 = 4;
+        int a2 = 4;
+       
+        int kali = a1 * a2;
+        int bagi = a1 / a2;
+        System.out.println("===OPERASI ARITMATIKA * / ===");
+        System.out.println("perkalian : " + kali);
+        System.out.println("pembagian : " + bagi);
+    }
+}
+
+
