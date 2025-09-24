a7a66fc95562d85a4467abb0b831d9b62e960b69
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-24 07:29:39 +0700

    Add files via upload

---
+++ Day17.java
@@ -0,0 +1,17 @@
+
+package pkg100days;
+
+public class Day17 {
+   
+    public static void main(String[] args) {
+        int c1 = 18;
+        int c2 = 6;
+       
+        int sisabagi = c1 % c2;
+        System.out.println("===OPERASI ARITMATIKA % ===");
+        System.out.println("perkalian : " + sisabagi);
+       
+    }
+}
+
+
