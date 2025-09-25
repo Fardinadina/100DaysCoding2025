3ecbaea7b87d001ef9b98597b63a387e609abd12
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-25 20:31:56 +0700

    Add files via upload

---
+++ Day18.java
@@ -0,0 +1,12 @@
+package pkg100days;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        int angka = 19;        // tipe data kecil (int)
+        double hasil = angka;  // otomatis: int → double
+
+        System.out.println("Nilai int\t: " + angka);
+        System.out.println("Nilai double\t: " + hasil);
+    }
+}
