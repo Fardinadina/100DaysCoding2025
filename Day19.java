544218f8fe8e462a818eaf89f0567ce4ddc83903
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-09-26 13:14:47 +0700

    Day19.java

--- Day19.java
+++ Day19.java
@@ -3,10 +3,11 @@ package pkg100days;
 public class Day19 {
 
     public static void main(String[] args) {
-        double abesar = 11.55;          // tipe data besar (double)
+        double abesar = 12.75;          // tipe data besar (double)
         int akecil = (int) abesar;      // konversi manual: double → int
 
         System.out.println("Nilai double\t: " + abesar);
         System.out.println("Nilai int\t: " + akecil);
     }
 }
+
