6c2608dfbf1f0b08e29338724067b0b9dbd58df3
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-02 22:09:44 +0700

    Add files via upload

---
+++ Day25.java
@@ -0,0 +1,17 @@
+package pkg100days;
+
+public class Day25 {
+    public static void main(String[] args) {
+        System.out.println("====INCREMENT====");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("tampilkan increment: " + i);
+        }
+        System.out.println("====DECREMENT====");
+        for (int j = 10; j > 1; j--) {
+            System.out.println("tampilkan decrement: " + j);
+            
+            
+        
+    }
+    }
+}
