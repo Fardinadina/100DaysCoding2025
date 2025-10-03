f1add1c9a0dc27f3dc9f54619b20049d4bcef41a
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-03 20:35:38 +0700

    Add files via upload

---
+++ Day26.java
@@ -0,0 +1,24 @@
+package pkg100days;
+
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(" nilai a normal : "+ a);
+        
+        a += 5;
+        System.out.println(" setelah a += 5  : "+ a);
+        
+        a -= 5;
+        System.out.println(" setelah a -= 5   : "+ a);
+        
+        a *= 5;
+        System.out.println(" setelah a *= 5   : "+ a);
+        
+        a /= 5;
+        System.out.println(" setelah a /= 5   : "+ a);
+        
+        a %= 5;
+        System.out.println(" setelah a %= 5   : "+ a);
+        
+    }
+}
