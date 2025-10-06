ae824f1c314f8f5af5b7c01fef67606a65484b37
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-06 20:58:54 +0700

    Add files via upload

---
+++ Day29.java
@@ -0,0 +1,12 @@
+package pkg100days;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 1;
+        System.out.println(a < b);
+        System.out.println(a > b);
+
+    }
+}
