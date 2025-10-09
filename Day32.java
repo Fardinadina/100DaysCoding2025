87ea7e6a6f62490a5ebff9a79a5a3f00d7e86f23
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-09 22:08:06 +0700

    Add files via upload

---
+++ Day32.java
@@ -0,0 +1,12 @@
+package pkg100days;
+
+public class Day32 {
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 1;
+        System.out.println(a <= b || a >= b );
+        System.out.println(a <= a || b >= b );
+        System.out.println(a < a || b > b );
+
+    }}
