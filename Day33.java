8f80d58ab6b1fff9d48ef8b09873e8bb4575668d
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-10 21:51:48 +0700

    Add files via upload

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package pkg100days;
+
+public class Day33 {
+
+    public static void main(String[] args) {
+        boolean t = true;
+        boolean f = false;
+
+        System.out.println("ini kebalikan dari true : " + !t);
+        System.out.println("ini kebalikan dari false :" + !f);
+    }
+}
