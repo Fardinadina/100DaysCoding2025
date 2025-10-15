a9c409302bbc053e5173d974a33e4af9c693cea4
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-15 22:16:33 +0700

    Day38.java

--- Day38.java
+++ Day38.java
@@ -6,7 +6,7 @@ public class Day38 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("=== Program Menentukan Bilangan Positif, Negatif, atau Nol ===");
+        System.out.println("=== Program Menentukan Bilangan Positif, Negatif, dan Nol ===");
         System.out.print("Masukkan sebuah bilangan: ");
         int bilangan = input.nextInt();
 
@@ -21,3 +21,4 @@ public class Day38 {
         input.close();
     }
 }
+
