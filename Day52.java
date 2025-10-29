cc65785ac4a041d641c2d7e5c53dca417ea84ef9
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-29 19:09:17 +0800

    Add files via upload

---
+++ Day52.java
@@ -0,0 +1,15 @@
+package pkg100days;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println("ini perulangan ke : " + i);
+            i++;
+
+        }
+    }
+
+}
