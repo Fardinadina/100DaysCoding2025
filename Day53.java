4d9eed63265d477c018bd4424db67e6afeacdd6c
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-30 23:03:38 +0800

    Add files via upload

---
+++ Day53.java
@@ -0,0 +1,14 @@
+package pkg100days;
+
+public class Day53 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                break; 
+            }
+            System.out.println(i);
+        }
+    }
+}
