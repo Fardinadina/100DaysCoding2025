69663e26ae1a204268e967dfacf9dd1d404330ad
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-10-31 20:48:44 +0800

    Day54.java

---
+++ Day54.java
@@ -0,0 +1,14 @@
+package pkg100days;
+
+public class Day54 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 4) {
+                continue; 
+            }
+            System.out.println(i);
+        }
+    }
+}
