edd2deb933543921006e59516de3eb1de993ceac
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-11-07 23:30:40 +0800

    Add files via upload

---
+++ Day61.java
@@ -0,0 +1,33 @@
+package pkg100days;
+
+import java.util.Scanner;
+
+public class Day61 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        System.out.print("Masukkan nilai M: ");
+        int M = input.nextInt();
+
+        System.out.println("Bilangan kelipatan " + M + " dari 1 hingga " + N + " :");
+
+        for (int i = 1; i <= N; i++) {
+            if (i % M == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+/**
+ *
+ * @author Computer
+ */
+public class Day61 {
+    
+}
