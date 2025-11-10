a9133904e387ae4c094fe9dce12903b64e39c53c
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-11-10 21:59:30 +0800

    Add files via upload

---
+++ Day64.java
@@ -0,0 +1,25 @@
+package pkg100days;
+
+import java.util.Scanner;
+
+public class Day64 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai M: ");
+        int M = input.nextInt();
+
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        int hasil = 1;
+
+        for (int i = 1; i <= N; i++) {
+            hasil = hasil * M;
+        }
+
+        System.out.println("Hasil dari " + M + " pangkat " + N + " adalah: " + hasil);
+    }
+}
