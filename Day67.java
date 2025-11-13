d0a47b6c79ba81c08f7018df41cf19e794e4fd6f
Fardina <147162916+Fardinadina@users.noreply.github.com>
2025-11-13 19:45:21 +0800

    Add files via upload

---
+++ Day67.java
@@ -0,0 +1,17 @@
+package pkg100days;
+
+import java.util.Scanner;
+
+public class Day67 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print("*");
+        }
+    }
+}
